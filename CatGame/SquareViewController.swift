578da3f16dd4ea7a81bacb53d108199cf708dc5d
AlexParkhom91 <107427927+AlexParkhom91@users.noreply.github.com>
2022-06-27 15:51:42 +0300

    Update SquareViewController.swift

--- CatGame/SquareViewController.swift
+++ CatGame/SquareViewController.swift
@@ -9,74 +9,76 @@ import UIKit
 
 class SquareViewController: UIViewController {
     
+        var SquareButton: UIButton = UIButton()
+    var Square: UILabel = UILabel()
+       
+       override func viewDidLoad() {
+           super.viewDidLoad()
+           view.backgroundColor = randomColor()
+       }
+       
+       override func viewDidAppear(_ animated: Bool) {
+           super.viewDidAppear(animated)
+           
+           // Button
+           let widthBtn = 100
+           let heightBtn = 100
+           let xBtn = (Int(view.bounds.width) / 2) - (widthBtn / 2)
+           let yBtn = Int(view.bounds.height) - (heightBtn * 2)
+           
+           SquareButton.frame = CGRect(x: xBtn, y: yBtn, width: widthBtn, height: heightBtn)
+           SquareButton.backgroundColor = randomColor()
+           SquareButton.layer.cornerRadius = SquareButton.bounds.height / 2
+           SquareButton.clipsToBounds = true
+           SquareButton.addTarget(self, action: #selector(createFieldSquares), for: .touchUpInside)
+           SquareButton.setTitle("Squares", for: .normal)
+           
+           view.addSubview(SquareButton)
+           
+       }
+       /// Creating field for Squares
+       @objc func createFieldSquares() {
+           
+           let columnsCount = 3
+           
+           let widthSquare = Int(view.bounds.width) / columnsCount
+           let heightSquare = widthSquare
+           let rowsCount = Int((Int(view.bounds.height) - 200) / heightSquare)
+           
+           var x = 0
+           var y = 0
+           
+           for _ in 1...(columnsCount * rowsCount) {
+               
+               let Square: UIView = UIView()
+               Square.frame = CGRect(x: x, y: y, width: widthSquare, height: heightSquare)
     
-        
-    let label1: UILabel = UILabel()
-        let label2: UILabel = UILabel()
-        let label3: UILabel = UILabel()
-        let label4: UILabel = UILabel()
-        let label5: UILabel = UILabel()
-        let label6: UILabel = UILabel()
-        let label7: UILabel = UILabel()
-        let label8: UILabel = UILabel()
-
-        public override func viewDidLoad() {
-            super.viewDidLoad()
-            view.backgroundColor = .orange
+               Square.backgroundColor = randomColor()
+               x += widthSquare
+               
+               if x >= Int(view.bounds.width) {
+                   x = 0
+                   y += heightSquare
+               }
+               view.addSubview(Square)
+               
+           }
+           
+       }
+    func randomColor() -> UIColor {
+        switch Int.random(in: 1...8) {
+        case 1: return UIColor.red
+        case 2: return UIColor.systemCyan
+        case 3: return UIColor.orange
+        case 4: return UIColor.brown
+        case 5: return UIColor.blue
+        case 6: return UIColor.systemFill
+        case 7: return UIColor.systemMint
+        case 8: return UIColor.yellow
+        default: return UIColor.systemRed
         }
-        override public func viewWillAppear(_ animated: Bool) {
-            super.viewWillAppear(animated)
-            label1.frame = CGRect(x: 20, y: 20, width: 180, height: 180)
-            view.addSubview(label1)
-            label1.backgroundColor = .blue
-            label2.frame = CGRect(x: 20, y: 20, width: 180, height: 180)
-            view.addSubview(label2)
-            label2.backgroundColor = .darkGray
-            label3.frame = CGRect(x: 20, y: 20, width: 180, height: 180)
-            view.addSubview(label3)
-            label3.backgroundColor = .red
-            label4.frame = CGRect(x: 20, y: 20, width: 180, height: 180)
-            view.addSubview(label4)
-            label4.backgroundColor = .systemGreen
-            label5.frame = CGRect(x: 20, y: 20, width: 180, height: 180)
-            view.addSubview(label5)
-            label5.backgroundColor = .black
-            label6.frame = CGRect(x: 20, y: 20, width: 180, height: 180)
-            view.addSubview(label6)
-            label6.backgroundColor = .systemCyan
-            label7.frame = CGRect(x: 20, y: 20, width: 180, height: 180)
-            view.addSubview(label7)
-            label7.backgroundColor = .brown
-            label8.frame = CGRect(x: 20, y: 20, width: 180, height: 180)
-            view.addSubview(label8)
-            label8.backgroundColor = .white
-        }
-    override public func viewDidAppear(_ animated: Bool) {
-            super.viewDidAppear(animated)
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + .microseconds(800)) {
-        self.label1.center = CGPoint(x: 110, y: 120)
-        }
-        DispatchQueue.main.asyncAfter(deadline: .now() + .microseconds(100)){
-        self.label2.center = CGPoint(x: 310, y: 120)
-        }
-        DispatchQueue.main.asyncAfter(deadline: .now() + .microseconds(1300)) {
-        self.label3.center = CGPoint(x: 110, y: 320)
-        }
-      DispatchQueue.main.asyncAfter(deadline: .now() + .microseconds(1700)){
-        self.label4.center = CGPoint(x: 310, y: 320)
-        }
-       DispatchQueue.main.asyncAfter(deadline: .now() + .microseconds(300)) {
-        self.label5.center = CGPoint(x: 110, y: 520)
     }
-        DispatchQueue.main.asyncAfter(deadline: .now() + .microseconds(1900)) {
-        self.label6.center = CGPoint(x: 310, y: 520)
-    }
-        DispatchQueue.main.asyncAfter(deadline: .now() + .microseconds(500)) {
-        self.label7.center = CGPoint(x: 110, y: 720)
-    }
-        DispatchQueue.main.asyncAfter(deadline: .now() + .microseconds(1700)) {
-        self.label8.center = CGPoint(x: 310, y: 720)
-            }
-    }
+    
 }
+
+
