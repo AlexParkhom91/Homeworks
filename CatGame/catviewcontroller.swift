336269c2b4233658bc2f45956de10ff92e30fb18
AlexParkhom91 <107427927+AlexParkhom91@users.noreply.github.com>
2022-06-27 13:02:45 +0300

    Update catviewcontroller.swift

--- CatGame/catviewcontroller.swift
+++ CatGame/catviewcontroller.swift
@@ -16,7 +16,7 @@ public final class CatViewController: UIViewController {
     movedButton.addTarget(self, action: #selector(moveBall(sender:)), for: .touchUpInside)
     view.addSubview(movedButton)
     movedButton.center = view.center
-    movedButton.backgroundColor = .red
+    movedButton.backgroundColor = randomColor()
 }
 
 @objc func moveBall(sender: UIButton) {
@@ -26,5 +26,17 @@ public final class CatViewController: UIViewController {
         sender.center = CGPoint (x: randomWidth, y: randomHeight)
    
 }
-   
+     func randomColor() -> UIColor {
+        switch Int.random(in: 1...8) {
+        case 1: return UIColor.red
+        case 2: return UIColor.systemCyan
+        case 3: return UIColor.orange
+        case 4: return UIColor.brown
+        case 5: return UIColor.blue
+        case 6: return UIColor.systemFill
+        case 7: return UIColor.systemMint
+        case 8: return UIColor.yellow
+        default: return UIColor.systemRed
+        }
+     }
 }
